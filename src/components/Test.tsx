5e97cd4e8719e9e9fdcc0fdfaf0a220bc377bb6b
nablsi14 <nathaniel.b.sigafoos@gmail.com>
2018-05-23 14:44:27 -0400

    Removed unused Test component

--- src/components/Test.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from "react";
-
-export interface ITestProps { compiler: string; framework: string; };
-
-export const Test = (props: ITestProps) => <h1>Testing {props.compiler} and {props.framework}</h1>;
