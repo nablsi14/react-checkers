f2866bc537c8f8e579fbbc5515d6b1e27301e360
nablsi14 <nathaniel.b.sigafoos@gmail.com>
2018-06-14 00:21:22 -0400

    Split MenuItem component into 3 smaller components

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,10 +1,8 @@
-import React, { Component, CSSProperties } from 'react';
-import * as FontAwesome from "react-icons/lib/fa";
-import { Link } from 'react-router-dom';
-import { Button, Col, ListGroupItem, Row } from 'reactstrap';
-
+import React, { Component} from 'react';
+import { Col, ListGroupItem, Row } from 'reactstrap';
 import { IGameInfo } from '../sharedTypes';
-import formatDate from '../util/formatDate';
+import MenuItemButtons from './MenuItemButtons';
+import SavedGameInfo from './SavedGameInfo';
 
 interface IMenuItemProps {
     deleteGame: (index: number) => void;
@@ -18,22 +16,12 @@ interface IMenuItemState {
 
 export default class MenuItem 
         extends Component<IMenuItemProps, IMenuItemState> {
-    private linkStyles: CSSProperties = {
-        color: "white", 
-        display: "block", 
-        height: "100%",
-        margin: "4px 0px",
-        textDecoration: "none"
+
+    public state: IMenuItemState = {
+        showButtons: false
     };
 
-    constructor (props: IMenuItemProps) {
-        super(props);
-        this.state = {
-            showButtons: false
-        };
-    }
     public render () {
-        const { info } = this.props;
         const deleteGame = () => this.props.deleteGame(this.props.index);
         const mouseEnter = () => this.setState({showButtons: true});
         const mouseLeave = () => this.setState({showButtons: false})
@@ -44,41 +32,13 @@ export default class MenuItem
                 style={ {display:"inline-block", width: "100%", marginBottom: "-4px"} }>
                 <Row>
                     <Col xs={8}>
-                        <h3>
-                            <span style={{color:"black", fontStyle: info.turn === 1 ? "italic" : "normal"}}>
-                                {info.p1.name}
-                            </span>
-                            <span className='small'> vs </span>
-                            <span style={{color: "red", fontStyle: info.turn === 2 ? "italic" : "normal"}}>
-                                {info.p2.name}
-                            </span>
-                        </h3>
-                        <h4>Score:{' '}
-                            <span style={{color:"black"}}>{info.p1.score}</span>:
-                            <span style={{color: "red"}}>{info.p2.score}</span>
-                        </h4>
-                        <p>
-                            Created: {formatDate(new Date(info.created))} {' '}
-                            Last played: {formatDate(new Date(info.last))}
-                        </p>
+                        <SavedGameInfo info={ this.props.info }/>
                     </Col>
                     <Col xs={4}>
-                        {this.state.showButtons && <div>
-                            <Link to={ {pathname:"/play", search:`?index=${this.props.index}`} } style={ this.linkStyles} >
-                                <Button block={ true }
-                                    color="success" >
-                                    Resume Game {' '}
-                                    <FontAwesome.FaShareSquare />
-                                </Button>
-                            </Link>
-                            <Button block={ true }
-                                color="danger"
-                                className='delete'
-                                onClick={ deleteGame } >
-                                Delete Game {" "}
-                                <FontAwesome.FaTrash />
-                            </Button>
-                        </div>}
+                        <MenuItemButtons 
+                            deleteGame={ deleteGame }
+                            index={ this.props.index }
+                            showButtons={ this.state.showButtons } />
                     </Col>
                 </Row>
             </ListGroupItem>
