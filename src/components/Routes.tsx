5717cefd6aed4f900da933ee5354dd4c38dba173
nablsi14 <nathaniel.b.sigafoos@gmail.com>
2018-08-09 15:59:32 -0400

    Dynamically import GameContainer and HowToPlay components in Routes

--- src/components/Routes.tsx
+++ src/components/Routes.tsx
@@ -1,14 +1,22 @@
 import React from "react";
+import Loadable from "react-loadable";
 import { Redirect, Route, Switch } from "react-router-dom";
-import GameContainer from "../components/Game/GameContainer";
 import MenuContainer from "../components/Menu/MenuContainer";
-import HowToPlay from "./HowToPlay/HowToPlay";
+import Loading from "./Loading";
 
+const AsyncGameContainer = Loadable({
+    loader: () => import("./Game/GameContainer"),
+    loading: Loading
+});
+const AsyncHowToPlay = Loadable({
+    loader: () => import("./HowToPlay/HowToPlay"),
+    loading: Loading
+});
 const Routes = () => (
     <Switch>
         <Route path="/menu" exact={true} component={MenuContainer} />
-        <Route path="/play/:index" component={GameContainer} />
-        <Route path="/howtoplay" component={HowToPlay} />
+        <Route path="/play/:index" component={AsyncGameContainer} />
+        <Route path="/howtoplay" component={AsyncHowToPlay} />
         <Redirect to="/menu" />
     </Switch>
 );
